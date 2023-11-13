d5a168ae421880c6dd7a37ce9900f99bde3ac9c9
Dragonixj <delafiado@gmail.com>
2023-11-13 06:33:56 -0800

    *Task

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,11 +10,13 @@ size_t recursive_height(const binary_tree_t *tree)
 {
 	size_t left = 0;
 	size_t right = 0;
+	
 	if (tree == NULL)			
 		return (0);
 
 	left = recursive_height(tree->left);
 	right = recursive_height(tree->right);
+
 	if (left > right)
 		return (left + 1);
 
